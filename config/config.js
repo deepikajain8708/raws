66553eeaa78b4ee542057434ffe87e1e3a640125
pilani <rbpilani@gmail.com>
2013-01-08 18:39:57 +0530

    integrating google auth

---
+++ config/config.js
@@ -0,0 +1,9 @@
+var config = new Object();
+config["GOOGLE_CLIENT_ID"]='473900949203.apps.googleusercontent.com';
+config["GOOGLE_CLIENT_SECRET"]='IVwtBmq1PN2an8H-akm9nclN';
+config["mongohost"]="localhost";
+config["dbname"]="praws";
+config["BASE_DATA_PATH"]="/home/pradeep/.nraws/";
+
+
+exports.cfg=config;
--- server.js
+++ server.js
@@ -1,49 +1,47 @@
-var mongoose = require('mongoose')
-      , Schema = mongoose.Schema
-
-,express = require('express')
-var passport = require('passport')
-  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
-var UserSchema = new Schema({}),User;
+var express = require('express'),mongoose = require('mongoose')
+  , passport = require('passport'),Schema = mongoose.Schema
+  , util = require('util'),stylus = require('stylus'),nib=require('nib')	
+  , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,logger= require('./logger.js'),
+,authhelper = require('./auth/authhelper.js'),router = require('./router.js');
 
 
- 
-var app = express();
-app.use(express.bodyParser())
-app.use(express.cookieParser())
-app.use(express.session({ secret: 'esoognom'}))
-app.use(passport.initialize());
-app.use(passport.session());
 
+var app = express.createServer();
 
-passport.use(new GoogleStrategy({
-  clientId: '473900949203.apps.googleusercontent.com'
-            , clientSecret: 'IVwtBmq1PN2an8H-akm9nclN'
-
-   , callbackURL: 'http://localhost:3000/auth/google/callback'
-
-  },
-  function(identifier, profile, done) {
-    User.findOrCreate({ openId: identifier }, function(err, user) {
-      done(err, user);
-    });
-  }
-));
-
-app.get('/auth/google', passport.authenticate('google'));
-app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/',failureRedirect: '/login' }));
-
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+// configure Express
+  app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  //app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard c' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  authhelper.initialize(app);
+  app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
+
+router.setup(app);
+
 
 app.listen(3000);
 
 
 
 
-
+function loggit(mess){
+logger.log(mess);
+}
 
